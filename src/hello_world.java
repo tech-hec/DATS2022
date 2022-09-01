f4dc1975725e17e515f0941a60e3ab12a202e37c
hector <hectorp2710@gmail.com>
2022-09-01 12:52:11 +0200

    Initial commit

---
+++ src/hello_world.java
@@ -0,0 +1,10 @@
+public class hello_world {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        System.out.println("Teller til ti:");
+        for (int i = 1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
